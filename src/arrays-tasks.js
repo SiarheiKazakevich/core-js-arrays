fc447d06515a6bbdf3de18cb04452a40b74073ec
SiarheiKazakevich <dianrom@tut.by>
2024-01-08 23:04:25 +0300

    feat: finished getArrayOfStrings

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -82,8 +82,10 @@ function getArrayOfPositives(arr) {
  *    [ 1, 2, 3, 4, 5 ] => []
  *    [ 'cat, 'dog', 'raccoon' ] => [ 'cat', 'dog', 'raccoon' ]
  */
-function getArrayOfStrings(/* arr */) {
-  throw new Error('Not implemented');
+function getArrayOfStrings(arr) {
+  return arr.filter(function isString(item) {
+    return typeof item === 'string';
+  });
 }
 
 /**
