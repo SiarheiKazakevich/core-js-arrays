d80f760faf615fcab9c8fd0001dbb73d3f8d3f68
Siarhei Kazakevich <dianrom@tut.by>
2025-01-01 22:47:53 +0300

    feat:  finish  getFalsyValuesCount-task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -381,8 +381,13 @@ function getElementByIndices(/* arr, indices */) {
  *  getFalsyValuesCount([ -1, 'false', null, 0 ]) => 2
  *  getFalsyValuesCount([ null, undefined, NaN, false, 0, '' ]) => 6
  */
-function getFalsyValuesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getFalsyValuesCount(arr) {
+  return arr.reduce(function getfalsy(summ, item) {
+    if (!item) {
+      return summ + 1;
+    }
+    return summ;
+  }, 0);
 }
 
 /**
